9aa6048de03a76d35f27f501a3fd6b3397e8c4ff
redgad88 <vadim_kuzin@list.ru>
2023-06-03 10:46:18 +0300

    2.1. Преобразование даты [#289476 # [#289476]]

--- src/main/java/ru/job4j/grabber/utils/HabrCareerDateTimeParser.java
+++ src/main/java/ru/job4j/grabber/utils/HabrCareerDateTimeParser.java
@@ -1,14 +1,12 @@
 package ru.job4j.grabber.utils;
 
 import java.time.LocalDateTime;
-import java.time.OffsetDateTime;
 import java.time.format.DateTimeFormatter;
 
 public class HabrCareerDateTimeParser implements DateTimeParser {
     @Override
     public LocalDateTime parse(String parse) {
-        OffsetDateTime current = OffsetDateTime.parse(parse);
-        DateTimeFormatter formatter = DateTimeFormatter.ISO_LOCAL_DATE_TIME;
-        return LocalDateTime.parse(current.format(formatter));
+        DateTimeFormatter formatter = DateTimeFormatter.ISO_OFFSET_DATE_TIME;
+        return LocalDateTime.parse(parse, formatter);
     }
 }
